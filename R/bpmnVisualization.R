f195f3c04b5a11df8f641ee77136335a2845c787
Celine Souchet <celine.souchet@bonitasoft.com>
2021-08-25 17:46:02 +0200

    Init widget

--- R/bpmnVisualization.R
+++ R/bpmnVisualization.R
@@ -1,15 +1,52 @@
-## import operator
-#' @importFrom data.table ":="
-NULL
-
-#' @importFrom magrittr "%>%"
-NULL
-
-####
-#' function bpmn visualization: entry point
+#' <Add Title>
 #'
-#' @return 'hello'
+#' <Add Description>
 #'
-bpmnVisualization <- function() {
-  returnValue('hello')
+#' @import htmlwidgets
+#'
+#' @export
+bpmnVisualization <- function(message, width = NULL, height = NULL, elementId = NULL) {
+
+  # forward options using x
+  x = list(
+    message = message
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'bpmnVisualization',
+    x,
+    width = width,
+    height = height,
+    package = 'bpmnVisualization',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for bpmnVisualization
+#'
+#' Output and render functions for using bpmnVisualization within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a bpmnVisualization
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name bpmnVisualization-shiny
+#'
+#' @export
+bpmnVisualizationOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'bpmnVisualization', width, height, package = 'bpmnVisualization')
+}
+
+#' @rdname bpmnVisualization-shiny
+#' @export
+renderBpmnVisualization <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, bpmnVisualizationOutput, env, quoted = TRUE)
 }
