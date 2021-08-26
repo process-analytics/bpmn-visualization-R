31a047539fe4833a55facfbb1a69f6892f93cc31
Souchet Céline <4921914+csouchet@users.noreply.github.com>
2021-08-26 16:50:35 +0200

    [FEAT] Initialize the project (#21)

---
+++ R/bpmnVisualization.R
@@ -0,0 +1,15 @@
+## import operator
+#' @importFrom data.table ":="
+NULL
+
+#' @importFrom magrittr "%>%"
+NULL
+
+####
+#' function bpmn visualization: entry point
+#'
+#' @return 'hello'
+#'
+bpmnVisualization <- function() {
+  returnValue('hello')
+}
