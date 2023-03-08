45966fe2c7403414512fb68f805cef8e4c2cd8a2
Souchet Céline <celine.souchet@bonitasoft.com>
2023-03-08 16:12:14 +0100

    Format tests

--- tests/testthat/test-bpmnVisualizationR.R
+++ tests/testthat/test-bpmnVisualizationR.R
@@ -1,87 +1,40 @@
 test_that("display works", {
   res <- display(
-    system.file(
-      "examples/Email_Voting.bpmn",
-      package = "bpmnVisualizationR"
-    )
+    system.file("examples/Email_Voting.bpmn", package = "bpmnVisualizationR")
   )
-  expect_true(
-    inherits(
-      res,
-      "bpmnVisualizationR"
-    )
-  )
-  expect_true(
-    inherits(
-      res,
-      "htmlwidget"
-    )
-  )
-
+  
+  expect_true(inherits(res, "bpmnVisualizationR"))
+  expect_true(inherits(res, "htmlwidget"))
+  
   # Be sure that the xml is in the widget
   string <- paste(
     readLines(
-      system.file(
-        "examples/Email_Voting.bpmn",
-        package = "bpmnVisualizationR"
-      )
+      system.file("examples/Email_Voting.bpmn", package = "bpmnVisualizationR")
     ),
     collapse = " "
   )
-
-  expect_true(
-    grepl(
-      substr(string, 1000, 1010),
-      res$x$bpmnContent
-    )
-  )
-
-  expect_true(
-    grepl(
-      substr(string, 2000, 2010),
-      res$x$bpmnContent
-    )
-  )
+  
+  expect_true(grepl(substr(string, 1000, 1010), res$x$bpmnContent))
+  expect_true(grepl(substr(string, 2000, 2010), res$x$bpmnContent))
 })
 
 test_that("bpmnVisualizationROutput works", {
   res <- bpmnVisualizationROutput("xyz")
-  expect_true(
-    inherits(
-      res,
-      "shiny.tag.list"
-    )
-  )
-
-  expect_true(
-    grepl(
-      "bpmnVisualizationR",
-      res
-    )
-  )
+  
+  expect_true(inherits(res, "shiny.tag.list"))
+  expect_true(grepl("bpmnVisualizationR", res))
 })
 
 
 test_that("renderBpmnVisualizationR works", {
   skip_if_not_installed("shiny")
+  
   res <- renderBpmnVisualizationR({
     display(
-      system.file(
-        "examples/Email_Voting.bpmn",
-        package = "bpmnVisualizationR"
-      )
+      system.file("examples/Email_Voting.bpmn", package = "bpmnVisualizationR")
     )
   })
-  expect_true(
-    inherits(
-      res,
-      "shiny.render.function"
-    )
-  )
-  expect_true(
-    inherits(
-      res(),
-      "json"
-    )
-  )
+  
+  expect_true(inherits(res, "shiny.render.function"))
+  expect_true(inherits(res(), "json"))
 })
